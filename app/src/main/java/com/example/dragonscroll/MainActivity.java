7a3efa72a0fd48c7e2d3131ac85c41fb682b38bc
Josh <joshuaeduque@gmail.com>
2024-10-16 16:02:45 -0500

    Added bottom navigation bar and fragment container

--- app/src/main/java/com/example/dragonscroll/MainActivity.java
+++ app/src/main/java/com/example/dragonscroll/MainActivity.java
@@ -17,7 +17,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            // Using systemBars.bottom adds unnecessary padding to the BottomNavigationView
+            // I don't know why, but we're removing it for now
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
             return insets;
         });
     }
