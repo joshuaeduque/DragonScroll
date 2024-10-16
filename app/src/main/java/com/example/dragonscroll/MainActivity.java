53858f27808dbcaa927b5c4905581cb5a4c7f508
Josh <joshuaeduque@gmail.com>
2024-10-16 17:04:55 -0500

    Added bottom navigation functionality

--- app/src/main/java/com/example/dragonscroll/MainActivity.java
+++ app/src/main/java/com/example/dragonscroll/MainActivity.java
@@ -7,8 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,27 @@ public class MainActivity extends AppCompatActivity {
             // I don't know why, but we're removing it for now
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
             return insets;
+        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment fragment = null;
+
+            if(item.getItemId() == R.id.navigation_home)
+                fragment = new HomeFragment();
+            else if(item.getItemId() == R.id.navigation_profile)
+                fragment = new ProfileFragment();
+            else if(item.getItemId() == R.id.navigation_settings)
+                fragment = new SettingsFragment();
+
+            if(fragment == null)
+                return false;
+
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragmentContainerView, fragment);
+            transaction.commit();
+
+            return true;
         });
     }
 }
