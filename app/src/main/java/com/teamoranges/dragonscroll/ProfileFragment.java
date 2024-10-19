19b9293d5d86f6af777cd7a190a7de8d6d9b3795
Josh <joshuaeduque@gmail.com>
2024-10-19 02:41:56 -0500

    Added profile name editor on click

--- app/src/main/java/com/teamoranges/dragonscroll/ProfileFragment.java
+++ app/src/main/java/com/teamoranges/dragonscroll/ProfileFragment.java
@@ -1,5 +1,8 @@
 package com.teamoranges.dragonscroll;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +12,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 /**
@@ -49,6 +53,10 @@ public class ProfileFragment extends Fragment {
         return fragment;
     }
 
+    private Context context;
+    private String profileName;
+    private SharedPreferences sharedPrefs;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,6 +64,16 @@ public class ProfileFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        // Get Context
+        context = requireContext();
+
+        // Get SharedPrefs
+        sharedPrefs = context.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+
+        // Get user profile name from SharedPrefs or the default profile name
+        String defaultProfileName = getString(R.string.profile_name_default);
+        profileName = sharedPrefs.getString(getString(R.string.profile_name_key), defaultProfileName);
     }
 
     @Override
@@ -64,14 +82,45 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        // Get TextView with user profile name
         TextView profileTextView = view.findViewById(R.id.profileTextView);
+
+        // Set profile name
+        profileTextView.setText(profileName);
         profileTextView.setClickable(true);
 
-        profileTextView.setOnClickListener(view2 -> {
-            profileTextView.setCursorVisible(true);
-            profileTextView.setFocusableInTouchMode(true);
-            profileTextView.setInputType(InputType.TYPE_CLASS_TEXT);
-            profileTextView.requestFocus();
+        // Set profile name on click listener
+        profileTextView.setOnClickListener(pv -> {
+            // Create EditText for user input
+            final EditText editText = new EditText(context);
+            editText.setHint("Profile name");
+
+            // Create AlertDialog
+            AlertDialog.Builder alert = new AlertDialog.Builder(context)
+                    .setMessage("Enter a new profile name")
+                    .setView(editText);
+
+            // Set AlertDialog positive button onclick
+            alert.setPositiveButton("Save", (dialog, button) -> {
+                // Get text in EditText
+                String editedText = String.valueOf(editText.getText());
+
+                // Set text of TextView with user profile name
+                profileTextView.setText(editedText);
+
+                // Save new profile name to SharedPrefs
+                SharedPreferences.Editor editor = sharedPrefs.edit();
+                editor.putString(getString(R.string.profile_name_key), editedText);
+                editor.apply();
+            });
+
+            // Set AlertDialog negative button empty onclick
+            alert.setNegativeButton("Cancel", (dialog, button) -> {
+                // Do nothing
+            });
+
+            // Show the AlertDialog 
+            alert.show();
         });
 
         return view;
