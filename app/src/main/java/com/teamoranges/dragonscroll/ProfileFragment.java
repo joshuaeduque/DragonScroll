7aa171a872e05b8bbe5760bc340e45055599c090
Josh <joshuaeduque@gmail.com>
2024-10-19 01:41:37 -0500

    Make profile name editable

--- app/src/main/java/com/teamoranges/dragonscroll/ProfileFragment.java
+++ app/src/main/java/com/teamoranges/dragonscroll/ProfileFragment.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +62,18 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        TextView profileTextView = view.findViewById(R.id.profileTextView);
+        profileTextView.setClickable(true);
+
+        profileTextView.setOnClickListener(view2 -> {
+            profileTextView.setCursorVisible(true);
+            profileTextView.setFocusableInTouchMode(true);
+            profileTextView.setInputType(InputType.TYPE_CLASS_TEXT);
+            profileTextView.requestFocus();
+        });
+
+        return view;
     }
 }
