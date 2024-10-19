b153376f2a64e34f01b51171407167c8a2eae403
Josh <joshuaeduque@gmail.com>
2024-10-18 21:02:42 -0500

    Use NavigationUI without AppBarConfiguration

--- app/src/main/java/com/teamoranges/dragonscroll/MainActivity.java
+++ app/src/main/java/com/teamoranges/dragonscroll/MainActivity.java
@@ -1,6 +1,7 @@
 package com.teamoranges.dragonscroll;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -27,31 +32,15 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        // Handle bottom navigation
+        // Get BottomNavigationView
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        // Add an item selection listener to the bottom nav view
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-            Fragment fragment = null;
-            int itemId = item.getItemId();
 
-            // Item IDs aren't constant so we cant use a switch :(
-            if(itemId == R.id.navigation_home)
-                fragment = new HomeFragment();
-            else if(itemId == R.id.navigation_profile)
-                fragment = new ProfileFragment();
-            else if(itemId == R.id.navigation_settings)
-                fragment = new SettingsFragment();
+        // Get NavController from NavHostFragment
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navHostFragment);
+        NavController navController = navHostFragment.getNavController();
 
-            // Do nothing if no valid selection
-            if(fragment == null)
-                return false;
+        // AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.homeFragment, R.id.profileFragment, R.id.settingsFragment).build();
 
-            // Replace fragment view with selected fragment if selection valid
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            transaction.replace(R.id.fragmentContainerView, fragment);
-            transaction.commit();
-
-            return true;
-        });
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
