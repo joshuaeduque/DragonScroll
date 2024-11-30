dbfd6c6ff9de5486ff417c7d79bcd2648879e53e
Josh <joshuaeduque@gmail.com>
2024-11-30 17:44:10 -0600

    Cleaned BookAdapter

--- app/src/main/java/com/teamoranges/dragonscroll/BookAdapter.java
+++ app/src/main/java/com/teamoranges/dragonscroll/BookAdapter.java
@@ -16,16 +16,14 @@ import java.util.List;
 
 public class BookAdapter extends RecyclerView.Adapter<BookAdapter.BookViewHolder> {
 
-    private static final String TAG = "BookAdapter";
-
     private final List<Book> bookList;
-    private final OnBookClickListener onClickListener;
-    private final OnBookLongClickListener onLongClickListener;
+    private final OnBookClickListener onBookClickListener;
+    private final OnBookLongClickListener onBookLongClickListener;
 
-    public BookAdapter(List<Book> bookList, OnBookClickListener onClickListener, OnBookLongClickListener onLongClickListener) {
+    public BookAdapter(List<Book> bookList, OnBookClickListener onBookClickListener, OnBookLongClickListener onBookLongClickListener) {
         this.bookList = bookList;
-        this.onClickListener = onClickListener;
-        this.onLongClickListener = onLongClickListener;
+        this.onBookClickListener = onBookClickListener;
+        this.onBookLongClickListener = onBookLongClickListener;
     }
 
     @NonNull
@@ -40,20 +38,26 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.BookViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull BookViewHolder holder, int position) {
+        // Get book at position
         Book book = bookList.get(position);
+
+        // Set holder title and author from book
         holder.title.setText(book.getTitle());
         holder.author.setText(book.getAuthor());
 
+        // Set holder cover if book has a uri for it
         if (book.getCoverUri() != null && !book.getCoverUri().isEmpty()) {
             holder.cover.setImageURI(Uri.parse(book.getCoverUri()));
         }
 
+        // Set holder on book click listener
         holder.itemView.setOnClickListener(view -> {
-            onClickListener.onBookClick(book, position);
+            onBookClickListener.onBookClick(book, position);
         });
 
+        // Set holder on book long click listener
         holder.itemView.setOnLongClickListener(view -> {
-            return onLongClickListener.onBookLongClick(book, position);
+            return onBookLongClickListener.onBookLongClick(book, position);
         });
     }
 
@@ -69,6 +73,7 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.BookViewHolder
         public BookViewHolder(@NonNull View itemView) {
             super(itemView);
 
+            // Set holder views 
             title = itemView.findViewById(R.id.titleTextView);
             author = itemView.findViewById(R.id.authorTextView);
             cover = itemView.findViewById(R.id.profileImageView);
